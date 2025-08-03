4ba464a4623afbe9527a03a47ef6dc2dffb3099f
Pham Viet Anh <vietanhpham1908@gmail.com>
2025-08-03 12:34:32 +0700

    update

---
+++ app/components/Layout.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+// Layout.tsx
+import { Sidebar } from "./Sidebar";
+import { Navbar } from "./Navbar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <div className="flex pt-16">
+        <Sidebar />
+        <main className="flex-1 p-6 ml-64">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 import { Button } from "~/components/Button";
-import {Layout} from "~/components/Layout";
+import { Layout } from "~/components/Layout";
 
 export default function Home() {
   return (
